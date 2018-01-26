8812f834bb261d534f90cb0d53633012189fc90f
Piotr Sociński <psocinski@gmail.com>
2018-01-26 15:52:55 +0100

    Finished task "file system".

---
+++ index.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var colors = require('colors');
+
+fs.readdir('./', function(err, data) {
+
+	if (err) throw err;
+	console.log('Dane przed zapisem!'.yellow);
+	console.log(data);
+	
+	fs.writeFile('./dir.txt', data, function(err, data) {
+		
+		if (err) throw err;
+		console.log('Zapisano!'.red);
+
+	});
+	
+	fs.readFile('./dir.txt', 'utf-8', function(err, data) {
+
+		if (err) throw err;
+		console.log('Dane po zapisie'.green)
+        console.log(data);
+
+	});
+    
+});
